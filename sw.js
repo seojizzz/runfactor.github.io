f2adf6972e6df75fa68261a9e6e827f83bee8054
seojizzz <169005426+seojizzz@users.noreply.github.com>
2025-03-03 16:16:34 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          return caches.delete(cache);
+        })
+      );
+    })
+  );
+});
