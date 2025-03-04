49f7833e52a6bbcb4481754332f5b2dd8db01175
seojizzz <seojizzzzz@gmail.com>
2025-03-04 23:08:16 +0800

    asdf
    asdf

--- sw.js
+++ sw.js
@@ -2,13 +2,12 @@ self.addEventListener("install", (event) => {
     console.log("Service Worker installing.");
     self.skipWaiting();
 });
+
 self.addEventListener("activate", (event) => {
     console.log("Service Worker activated.");
     event.waitUntil(
         caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cache => caches.delete(cache))
-            );
+            return Promise.all(cacheNames.map(cache => caches.delete(cache)));
         })
     );
     self.clients.claim();
