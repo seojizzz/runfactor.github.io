e5e96e9279f8a9c2a1faf975d5826bb236d66d4b
seojizzz <seojizzzzz@gmail.com>
2025-03-05 23:16:37 +0800

    asdf

--- game.js
+++ game.js
@@ -1,8 +1,7 @@
 // 1. Import Firebase Modules
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-analytics.js";
-import { getFirestore, collection, addDoc, getDocs, serverTimestamp } 
-  from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+import { getFirestore, collection, addDoc, getDocs, serverTimestamp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 
 // 2a. Firebase Configuration
