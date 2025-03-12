394af282f4791bd99ea5dae4f1b28daac23478f2
Seojizzz <seojizzzzz@gmail.com>
2025-03-12 12:45:55 +0800

    a

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 // Assume you have a variable that tracks the score during the game:
 let currentScore = 0;
-const sheetURL = "https://script.google.com/macros/s/AKfycbyD1IWUr1d7v49a8w9c3K3gOEA6zpbMdEmiRy_tbSQdG1OJHZZGXDViev6alv02zqgg/exec";
+const sheetURL = "https://script.google.com/macros/s/AKfycby2Vp7FhEIQR6kvZoH6RF7jgcm376rfZGMrLiZ5bm5QDMPu6ci7uqweO0RbCI3KT4w1/exec";
 
 
 class PrimeFactorGame {
